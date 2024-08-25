99a5fb1d8291c9342ab3bdddfb819f71ad5f2f24
rashidulislamg71 <jrashidulislam0103@gmail.com>
2024-08-25 08:48:35 +0600

    contact form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/jsx-no-duplicate-props */
 /* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable no-unused-vars */
@@ -9,15 +10,81 @@ import { FaXTwitter } from "react-icons/fa6";
 import { ImFacebook2 } from "react-icons/im";
 import { FaWhatsappSquare } from "react-icons/fa";
 import contact from "..//../assets/images/contact/contact_me2.png";
-
-import { MdMarkEmailUnread } from "react-icons/md";
-import { FaSquarePhone } from "react-icons/fa6";
-
-import { IoShareSocialSharp } from "react-icons/io5";
-import { MdAddLocationAlt } from "react-icons/md";
-import { MdContactless } from "react-icons/md";
+import { useState } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    phone: "",
+    email: "",
+    address: "",
+    subject: "",
+    message: "",
+
+
+  });
+
+  const [errors, setErrors] = useState({
+    email: "",
+  });
+
+  const [responseMessage, setResponseMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+
+    setErrors({
+      ...errors,
+      [name]: "",
+    });
+  };
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let hasErrors = false;
+    const newErrors = {};
+
+    if (!validateEmail(formData.email)) {
+      newErrors.email = "একটি বৈধ ইমেইল ঠিকানা প্রবেশ করুন।";
+      hasErrors = true;
+    }
+
+    if (hasErrors) {
+      setErrors(newErrors);
+      return;
+    }
+
+    emailjs
+      .send("service_95qtmfk", "template_bpv24su", formData, "7xg82TxmrYm_fyH-6")
+      .then((response) => {
+        setResponseMessage("ইমেইল সফলভাবে পাঠানো হয়েছে!");
+        setFormData({
+          fullName: "",
+          phone: "",
+          email: "",
+          address: "",
+          subject: "",
+          message: "",
+        });
+      })
+      .catch((error) => {
+        setResponseMessage("ইমেইল পাঠাতে ব্যর্থ হয়েছে।");
+      });
+  };
+
+  console.log(formData)
+
   return (
     <>
       <div className={styles.contact_section}>
@@ -29,25 +96,41 @@ const Contact = () => {
               <div className={` ${styles.contact_greetings}`}>
                 <h3>Welcome Back in Contact!</h3>
                 <p>
-                  I am very excited to talk to you. <br />
-                  Feel Free to say. I am waiting for you.{" "}
+                  I'm very very excited to chat with you! I feel happiness{" "}
+                  <br /> when I talk to you. So, Feel free to share your
+                  thoughts or questions. <br />
+                  I'm always here and ready to assist with anything you need.
+                  <br />
+                  <span> Let's get started now! </span>
                 </p>
               </div>
               <div className={`${styles.flex} ${styles.left_bottom}`}>
                 <div className={`${styles.flex} ${styles.contact_icon}`}>
-                 <span> <FaSquarePhone /></span>
-               <span> <FaLinkedin /></span>
-                 <span> <FaSquareGithub /></span>
+                  <span>
+                    {" "}
+                    <FaWhatsappSquare />
+                  </span>
+                  <span>
+                    {" "}
+                    <FaSquareGithub />
+                  </span>
+                  <span>
+                    <FaLinkedin />
+                  </span>
+                  <span>
+                    <ImFacebook2 />
+                  </span>
                 </div>
                 <div className={`${styles.flex} ${styles.contact_way}`}>
                   <b>01782-242671</b>
-                  <b>Linkedin</b>
                   <b>GitHub</b>
+                  <b>Linkedin</b>
+                  <b>Facebook</b>
                 </div>
               </div>
             </div>
             <div className={styles.contact_right}>
-              <form className={styles.contact_form}>
+              <form className={styles.contact_form} onSubmit={handleSubmit}>
                 <div className={styles.form_group}>
                   <input
                     type="text"
@@ -55,6 +138,9 @@ const Contact = () => {
                     name="fullName"
                     required
                     placeholder="Full Name"
+                    autoFocus
+                    value={formData.fullName}
+                    onChange={handleChange}
                   />
                 </div>
 
@@ -65,6 +151,8 @@ const Contact = () => {
                     name="phone"
                     required
                     placeholder="Phone Number"
+                    value={formData.phone}
+                    onChange={handleChange}
                   />
                 </div>
 
@@ -75,7 +163,12 @@ const Contact = () => {
                     name="email"
                     required
                     placeholder="Email"
+                    value={formData.email}
+                    onChange={handleChange}
                   />
+                  {errors.email && (
+                    <p className={styles.error}>{errors.email}</p>
+                  )}
                 </div>
 
                 <div className={styles.form_group}>
@@ -85,22 +178,42 @@ const Contact = () => {
                     name="address"
                     required
                     placeholder="Address"
+                    value={formData.address}
+                    onChange={handleChange}
                   />
                 </div>
 
                 <div className={styles.form_group}>
+                  <input type="text"
+                  
+                    id={styles.message}
+                    name="subject"
+                    required
+                    placeholder="Subject"
+                    value={formData.subject}
+                    onChange={handleChange}
+                  />
+              
+                </div>
+
+                <div className={styles.form_group}>
                   <textarea
-                    id="message"
+                    id={styles.message}
                     name="message"
                     rows="5"
                     required
                     placeholder="Message"
+                    value={formData.message}
+                    onChange={handleChange}
                   ></textarea>
                 </div>
 
                 <button type="submit" className={styles.submit_btn}>
                   Send Message
                 </button>
+                {responseMessage && (
+                  <p className={styles.response}>{responseMessage}</p>
+                )}
               </form>
             </div>
           </div>
---
+++ src/Components/SocialContact/SocialContact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from "./SocialContact.module.css";
+const SocialContact = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SocialContact;
