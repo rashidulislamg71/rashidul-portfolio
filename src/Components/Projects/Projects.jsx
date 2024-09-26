eae7278b616446e260e8935e9a62955c991d9c92
rashidulislamg71 <jrashidulislam0103@gmail.com>
2024-09-26 08:30:20 +0600

    update

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import styles from "./Projects.module.css";
-import trust_farms from "../../assets/images/trust-farm.png";
+import trust_farms from "../../assets/images/Project_img/trust-farm.png";
+import cryptop_img from "../../assets/images/Project_img/Cryptop.png";
 import { IoArrowForwardSharp } from "react-icons/io5";
 import { IoMdArrowDropup } from "react-icons/io";
 
@@ -7,30 +8,6 @@ const Projects = () => {
   return (
     <section className={styles.projects_section}>
       <h3 className={styles.title}>My Projects</h3>
-      <div className={styles.project_category}>
-        <ul className={styles.flex}>
-          <li>
-            <a href="">
-              E-commerce
-            </a>
-          </li>
-          <li>
-            <a href="">
-              Sites 
-            </a>
-          </li>
-          <li>
-            <a href="">
-              Landing Page 
-            </a>
-          </li>
-          <li>
-            <a href="">
-              Email Template 
-            </a>
-          </li>
-        </ul>
-      </div>
       <div className={styles.projects_container}>
         <div className={`${styles.projects_content} ${styles.flex}`}>
           <div className={styles.single_project}>
@@ -41,10 +18,33 @@ const Projects = () => {
               <div className={styles.project_text}>
                 <h3>Trust Farms (E-commerce)</h3>
                 <div className={`${styles.project_links} ${styles.flex}`}>
-                  <a href="" target="_blank">
+                  <a href="https://trust-farms.vercel.app/" target="_blank">
                     Live Preview
                   </a>
-                  <a href="" target="_blank">
+                  <a href="https://github.com/rashidulislamg71/trust-farms" target="_blank">
+                    GitHub Link
+                  </a>
+                </div>
+                <div className={`${styles.flex} ${styles.project_details_btn}`}>
+
+                <button>More Project Details</button>
+                <span><IoArrowForwardSharp /></span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.single_project}>
+            <div className={styles.project_detailes}>
+              <div className={styles.project_img}>
+                <img src={cryptop_img} alt="cryptop" />
+              </div>
+              <div className={styles.project_text}>
+                <h3>Cryptop (Template)</h3>
+                <div className={`${styles.project_links} ${styles.flex}`}>
+                  <a href="https://cryptopcom.vercel.app/" target="_blank">
+                    Live Preview
+                  </a>
+                  <a href="https://github.com/rashidulislamg71/cryptop.com" target="_blank">
                     GitHub Link
                   </a>
                 </div>
